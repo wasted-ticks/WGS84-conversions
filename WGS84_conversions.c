8bb518929a9d2b67bb05a7013d3dc8bd23b1f3e1
paper-19 <basque@protonmail.com>
2018-10-16 13:23:14 -0400

    finished WGS84_LLA_to_ECEF conversion.

--- WGS84_conversions.c
+++ WGS84_conversions.c
@@ -4,6 +4,10 @@
 #define WGS84_INV_FLATTENING (298.257223563)
 #define WGS84_FLATTENING (1 / WGS84_INV_FLATTENING)
 
+/**
+ * CONVERSION FUNCTIONS FOR WGS84 DATUM
+ */
+
 extern struct WGS84_LLA {
 	double lat;
 	double lng;
@@ -17,6 +21,10 @@ extern struct WGS84_ECEF {
 	double z;
 };
 
+/**
+ * CONVERTS COORDINATES OF (LAT, LNG, ALTITUDE) TO ECEF (X, Y, Z)
+ * USING WSG84 DATUM.
+ */
 extern struct WGS84_ECEF WGS84_LLA_to_ECEF
 	( struct WGS84_LLA lla )
 {
@@ -24,13 +32,18 @@ extern struct WGS84_ECEF WGS84_LLA_to_ECEF
 	
 	double rad = (M_PI / 180.0);
 	
-	//double c_lat = cos(rad * lla.lat);
-	//double s_lat = sin(rad * lla.lat);
+	double c_lat = cos(rad * lla.lat);
+	double s_lat = sin(rad * lla.lat);
 	
-	//double c_lng = cos(rad * lla.lng);
-	//double s_lng = sin(rad * lla.lng);
+	double c_lng = cos(rad * lla.lng);
+	double s_lng = sin(rad * lla.lng);
 	
-	//TODO
+	double pvr = prime_vertical_radius(lla.lat);
+	double e_sq = WGS84_FLATTENING * (2 - WGS84_FLATTENING);
+	
+	ecef.x = (pvr + (lla.altitude / 1000.00)) * c_lat * c_lng;
+	ecef.y = (pvr + (lla.altitude / 1000.00)) * c_lat * s_lng;
+	ecef.z = ((1.00 - e_sq) * pvr + (lla.altitude / 1000.00)) * s_lat;
 	
 	return ecef;
 }
@@ -44,3 +57,13 @@ extern struct WGS84_LLH WGS84_ECEF_to_LLA
 	
 	return lla;
 }
+
+static double prime_vertical_radius
+	(double lat)
+{
+	double rad = (M_PI / 180.0);
+	double s_lat = sin(rad * lat);
+	double e_sq = WGS84_FLATTENING * (2 - WGS84_FLATTENING);
+	return WGS84_RADIUS / sqrt(1.00 - e_sq * s_lat * s_lat);
+}
+
